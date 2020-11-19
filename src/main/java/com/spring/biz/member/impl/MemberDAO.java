fc461b2469c7c5aa1d93ea093305ef9c7f1d21e3
bitcamp <bitcamp@DESKTOP-GCP47N5>
2020-11-19 10:56:41 +0900

    add getMember(S_MemberVO)

--- src/main/java/com/spring/biz/member/impl/MemberDAO.java
+++ src/main/java/com/spring/biz/member/impl/MemberDAO.java
@@ -4,10 +4,15 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.spring.biz.member.MemberVO;
+import com.spring.biz.member.S_MemberVO;
 
 @Repository
 public class MemberDAO {
 	private SqlSessionTemplate mybatis;
+	
+	public S_MemberVO getMember(S_MemberVO svo) {
+		return mybatis.selectOne("memberDAO.getMember", svo);
+	}
 	
 	public int getId(String id) {
 		return mybatis.selectOne("memberDAO.getId", id);
