63d15d9e64f068bd89e1874a43177c48f97889ac
bitcamp <bitcamp@DESKTOP-GCP47N5>
2020-11-19 10:49:09 +0900

    add getMember() method

--- src/main/java/com/spring/biz/member/MemberService.java
+++ src/main/java/com/spring/biz/member/MemberService.java
@@ -2,8 +2,9 @@ package com.spring.biz.member;
 
 
 public interface MemberService {
-	public MemberVO getMember(MemberVO vo);
+//	public MemberVO getMember(MemberVO vo);
 	public int insertMember(MemberVO vo);
 	public MemberVO checkIdPwd(String id, String pwd);
 	public int checkId(String id);
+	public S_MemberVO getMember(S_MemberVO svo);
 }
