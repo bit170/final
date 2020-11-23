60491e28f601bba9d4e4ffd71a0bfba26d8421d6
yonnieg <yeonhee.gk@gmail.com>
2020-11-23 10:25:13 +0900

    JSP파일 WEB-INF 내로 경로 이동

---
+++ src/main/java/com/spring/biz/view/main/MainController.java
@@ -0,0 +1,23 @@
+package com.spring.biz.view.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.spring.biz.member.MemberVO;
+
+@Controller
+public class MainController {
+	
+	
+	
+	public MainController() {
+		System.out.println("mainController 객체 생성");
+	}
+
+	@RequestMapping("/main.do")
+	public String main() {
+		return "/WEB-INF/main/index.jsp";
+	}
+}
