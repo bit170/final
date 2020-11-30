49cde169540a164486b3c2ae940a7f585c0dc850
yonnieg <yeonhee.gk@gmail.com>
2020-11-30 19:22:11 +0900

    수정본?

---
+++ src/main/java/com/spring/biz/product/PImageFileVO.java
@@ -0,0 +1,63 @@
+package com.spring.biz.product;
+
+public class PImageFileVO {
+	private String p_code;
+	private String pi_filename;
+	private String pi_code;
+	private String pi_filetype;
+	private String id;
+	
+	public PImageFileVO() {}
+	
+	public PImageFileVO(String p_code, String pi_filename, String pi_code, String pi_filetype, String id) {
+		super();
+		this.p_code = p_code;
+		this.pi_filename = pi_filename;
+		this.pi_code = pi_code;
+		this.pi_filetype = pi_filetype;
+		this.id = id;
+		
+	}
+
+	public String getP_code() {
+		return p_code;
+	}
+
+	public void setP_code(String p_code) {
+		this.p_code = p_code;
+	}
+
+	public String getPi_filename() {
+		return pi_filename;
+	}
+
+	public void setPi_filename(String pi_filename) {
+		this.pi_filename = pi_filename;
+	}
+
+	public String getPi_code() {
+		return pi_code;
+	}
+
+	public void setPi_code(String pi_code) {
+		this.pi_code = pi_code;
+	}
+
+	public String getPi_filetype() {
+		return pi_filetype;
+	}
+
+	public void setPi_filetype(String pi_filetype) {
+		this.pi_filetype = pi_filetype;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	
+}
---
+++ src/main/java/com/spring/biz/product/ProductVO.java
@@ -0,0 +1,91 @@
+package com.spring.biz.product;
+
+import java.sql.Date;
+
+public class ProductVO {
+	private String p_code;
+	private String p_name;
+	private int price;
+	private String p_size;
+	private Date p_date;
+	private String p_category;
+	private String p_detail;
+	
+	
+	public ProductVO() {}
+
+	public ProductVO(String p_code, String p_name, int price, String p_size, Date p_date, String p_category, String p_detail) {
+		super();
+		this.p_code = p_code;
+		this.p_name = p_name;
+		this.price = price;
+		this.p_size = p_size;
+		this.p_date = p_date;
+		this.p_category = p_category;
+		this.p_detail = p_detail;
+	}
+	
+	public String getP_code() {
+		return p_code;
+	}
+
+	public void setP_code(String p_code) {
+		this.p_code = p_code;
+	}
+
+	public String getP_name() {
+		return p_name;
+	}
+
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getP_size() {
+		return p_size;
+	}
+
+	public void setP_size(String p_size) {
+		this.p_size = p_size;
+	}
+
+	public Date getP_date() {
+		return p_date;
+	}
+
+	public void setP_date(Date p_date) {
+		this.p_date = p_date;
+	}
+
+	public String getP_category() {
+		return p_category;
+	}
+
+	public void setP_category(String p_category) {
+		this.p_category = p_category;
+	}
+
+	public String getP_detail() {
+		return p_detail;
+	}
+
+	public void setP_detail(String p_detail) {
+		this.p_detail = p_detail;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductVO [p_code=" + p_code + ", p_name=" + p_name + ", price=" + price
+				+ ", p_size=" + p_size + ", p_date=" + p_date + ", p_category=" + p_category + ", p_detail=" + p_detail
+				+ "]";
+	}
+
+}
